a902e42bd7a10e7bd2f02c317eb61eb7a95c8a11
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2024-03-01 15:00:28 +0530

    My Second Commit

---
+++ src/Test_Package/FirstTest.java
@@ -0,0 +1,11 @@
+package Test_Package;
+
+public class FirstTest {
+    public void m1() {
+        System.out.println("m1 execuated");
+        System.out.println("New commit");
+    }
+    public void m2() {
+        System.out.println("m2 execuated");
+    }
+}
